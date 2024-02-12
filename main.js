6370ae6c79078cc72facd54fe72856a824edbd2c
Kay4wise <159776838+Kay4wise@users.noreply.github.com>
2024-02-12 12:40:06 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//nav background
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0)
+})
+
+//Filter
+$(document).ready(function () {
+    $(".filter-item").click(function () {
+        const value = $(this).attr("data-filter");
+        if(value == "all"){
+            
+        }
+    })
+
+})
